49868729bb70deca69b61f2222a135e5cd03c0f8
pranjalmalpani928 <pranjalmaheshwari928@gmail.com>
2024-04-15 21:02:43 +0530

    session_6

---
+++ housing/entity/config_entity.py
@@ -0,0 +1,16 @@
+from collections import namedtuple
+
+DataIngetionConfig=namedtuple('DataIngetionConfig',['dataset_url','tgz_download_url',
+                                                    'ingested_train_dir','ingested_test_dir'])
+
+DataValidationConfig=namedtuple('DataValidationConfig',['schema_file_path'])
+
+DataTransformationConfig=namedtuple('DataTransformationConfig',['add_bedroom_per_room','transformed_train_dir'
+                                                                'transformed_test_dir','preprocessed_object_file_path'])
+
+ModelTrainingConfig=namedtuple('ModelTrainingConfig',['train_model_file_path','base_accuracy'])
+
+ModelEvaluationConfig=namedtuple('ModelEvaluationConfig',['model_evaluation_file_path','timestamp'])
+
+ModelPusherConfig=namedtuple('ModelPusherConfig',['export_dir_path'])
+
