dffc4de061c7b65b28bf1f86aefe911056cc4752
pranjalmalpani928 <pranjalmaheshwari928@gmail.com>
2024-04-17 09:59:39 +0530

    session_8

--- housing/constants/constants.py
+++ housing/constants/constants.py
@@ -13,10 +13,26 @@ CONFIG_FILE_PATH=os.path.join(ROOT_DIR,CONFIG_DIR,CONFIG_FILE_NAME)
 
 
 
-# Training Pipelien Constants Declarations
+# Training Pipeline Constants Declarations
 
 TRAINING_PIPELINE_CONFIG_KEY='training_pipeline_config'
 TRAINING_PIPELINE_ARTIFACT_DIR_KEY='artifact_dir'
 TRAINING_PIPELINE_NAME_KEY='pipeline_name'
 
 
+# Constants for Data Ingetion configuration
+
+DATA_INGETION_CONFIG_KEY='data_ingetion_config'
+DATA_INGETION_ARTIFACT_DIR='data_ingetion'
+DATA_INGETION_DOWNLOAD_URL_KEY='dataset_download_url'
+DATA_INGETION_RAW_DATA_DIR_KEY='raw_data_dir'
+DATA_INGETION_TGZ_DOWNLOAD_URL_KEY='tgz_download_url'
+DATA_INGETION_DATA_DIR_NAME_KEY='ingested_data_dir'
+DATA_INGETION_TRAIN_DIR_NAME_KEY='ingetsted_train_dir'
+DATA_INGETION_TEST_DIR_NAME_KEY='ingested_test_dir'
+
+
+# Constants for Data Validation Configurations
+
+DATA_VALIDATION_CONFIG_KEY= 'data_validation_config'
+DATA_VALIDATION_SCHEMA_FILE_NAME_KEY= 'schema_file_name'
